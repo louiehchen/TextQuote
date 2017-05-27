1d7a3ab1e79bbb6e1677f30c75e6be3e691087fd
Louie <louiehchen@gmail.com>
2017-05-27 10:53:47 -0700

    add messenger module, required libraries, and added keys to gitignore

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,14 @@ require_relative 'boot'
 
 require 'rails/all'
 
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "sprockets/railtie"
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -11,5 +19,8 @@ module TextQuote
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.active_record.raise_in_transactional_callbacks = true
+    config.autoload_paths += %W(#{config.root}/lib)
   end
 end
