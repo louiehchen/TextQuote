e3e3d1b2b1199fb552692fe0e755147be32cc234
Louie <louiehchen@gmail.com>
2017-05-27 10:19:07 -0700

    added controller and model, adjusted routes, installed bootstrap

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 Simple app to text random quotes to you using Twilio
 
-* ...
+* 
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+	'root' => 'texts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :texts, :only => [:index]
+  post 'texts/send_text' => 'texts#send_text'
+
+
 end
