47fe601e007e226d7616a2ab56f7060c045fe577
Louie <louiehchen@gmail.com>
2017-05-29 10:34:54 -0700

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,5 @@ Rails.application.routes.draw do
   resources :texts, :only => [:index]
   post 'texts/send_text' => 'texts#send_text'
 
-	resources :phones, only: [:new, :create]
-	post 'phones/verify' => "phones#verify"
-	resources :phones, only: [:new, :create]
-	post 'phone_numbers/verify' => "phone_numbers#verify"
 
 end
